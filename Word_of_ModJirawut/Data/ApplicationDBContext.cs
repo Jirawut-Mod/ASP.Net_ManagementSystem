c533b1ce84d15fd0f29edef58a8be2228b33f338
Jirawut Pimsoda <mod.jirawut1681@gmail.com>
2023-08-20 00:24:48 +0700

    Add project files.

---
+++ Word_of_ModJirawut/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Word_of_ModJirawut.Models;
+
+namespace Word_of_ModJirawut.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options) :base(options){ 
+        
+        }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ Word_of_ModJirawut/Migrations/20230819081814_addProjectDB.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Word_of_ModJirawut.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProjectDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PriceUnit = table.Column<float>(type: "real", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
